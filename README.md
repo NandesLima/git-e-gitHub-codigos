92e5ca8e06f8a310a494638b43f68b2cf66a08df
NandesLima <euernandeslima@gmail.com>
2022-08-25 17:21:43 -0300

    Alterações no README.md: Chaves SSH. Alterações no git.sh: Códigos de SSH

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 - Dentro do git.sh estou colocando os códigos e algumas explicações.
 
-### Entendendo o funcionamento do Git
+## Entendendo o funcionamento do Git
 
-#### Sha1
+### Sha1
 
 Secure Hash Algorith (Algorítmo de hash seguro) - Algorítmo que faz o encriptamento de um arquivo, a encriptação gera um conjunto de 40 caracteres onde cada conjunto é único, e serve como identificação. Representa um arquivo de forma única.
 
@@ -12,11 +12,11 @@ Comando que gera um sha1 para o arquivo:
 
 ![](https://github.com/NandesLima/git-e-gitHub-codigos/blob/master/imagens/openssl-sha1.png)
 
-#### Objetos Internos do Git (Responsáveis pelo versionamento)
+### Objetos Internos do Git (Responsáveis pelo versionamento)
 
 Objetos são constituídos por metadados.
 
-##### Blobs (bolhas)
+#### Blobs (bolhas)
 
 Blobs são a composição básica dos objetos.
 
@@ -28,7 +28,7 @@ Metadados:
  - Conteúdo do blob
  - sha1 próprio
 
-##### Trees (Árvores)
+#### Trees (Árvores)
 
 São objetos que armazenam blobs e outras trees.
 
@@ -40,7 +40,7 @@ Metadados:
  - Guarda os nomes dos arquivos.
  - sha1 próprio
 
-##### Commit
+#### Commit
 
 Junta os blobs e trees e criam um projeto. E permite a vizualização da linha do tempo de alteração.
 
@@ -54,3 +54,14 @@ Metadados:
  - Possui um timestamp (Carimbo de tempo)
  - sha1 próprio
  
+## Chaves ssh
+
+### SSH
+
+Conexão segura entre duas máquinas com uma chave pública e chave privada.
+
+Para gerar as chaves no terminal entre o acesso remoto e o local:
+
+ - ssh-keygen -t ed25519 -C "e-mail"
+ - eval $(ssh-agent -s)
+ - ssh-add arquivo-do-id
--- git.sh
+++ git.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Comando para o repositório local
+
 openssl sha1 nome_do_arquivo # Guarda o versionamento e cria um código sha1
 
 git init  # Inicia o repositório dentro de um diretório local
@@ -10,10 +12,6 @@ git add nome_do_arquivo # Adiciona um arquivo para a área de staged
 git commit -m 'Mensagem do Commit' # Cria o commit com todos os arquivos na área de Staged deixando eles  com o status Unmodified
 				   # Um commit deixa as alterações rastreáveis.
 				   # O -m pede um comentário sobre as mudanças do commit.
-git push # Publica as alterações, os commits, do repositório local para o repositório remoto.
-git push origin master # Publicação feita pela branch master
-git pull # Atualiza o repositório local conforme o repositório remoto.
-git pull oringin master # Atualizar o repositório a partir do branch master
 git merge # Precisa juntar alterações de uma determinada branch com a master.
 git status # Mostra as informações de status do repositório local, Untracked, Unmodified, Modified, Staged
 git log # Mostra as alterações que o projeto vem sofrendo
@@ -21,7 +19,23 @@ git log # Mostra as alterações que o projeto vem sofrendo
 git branch nome_do_branch # Cria um novo ramo, o git init cria um branch master, o git branch cria ramificações.
 			  # Os branchs servem para partes diferentes dentro do projeto.
 			  # Visualiza todas as branchs
+
+
+# Comandos para gerar as chaves SSH - passo a passo
+
+ssh-keygen -t ed25519 -C 'e-mail'
+eval $(ssh-agent -s) # Cria um agente
+ssh-add arquivo_do_id # Associa a chave privada
+
+
+# Comandos do acesso remoto
+
 git remote add origin link_do_repositório_remoto # Cria uma conexão entre o repositório local e o remoto
 git remote - v # Mostra as conexões remotas que o repositório local tem
 git remote --verbose # Mostra as conexões remotas que o repositório local tem
 # Documentação: https://git-scm.com/docs
+git push # Publica as alterações, os commits, do repositório local para o repos>
+git push origin master # Publicação feita pela branch master
+git pull # Atualiza o repositório local conforme o repositório remoto.
+git pull oringin master # Atualizar o repositório a partir do branch master
+
